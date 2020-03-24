95e37714c4f0ad5bba163a103b46092ecac14899
Hisni <hisnionline@gmail.com>
2020-03-25 01:18:10 +0530

    Control Panel Added

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,32 +4,6 @@ import Controls from './Controls/Controls';
 import './Home.css';
 
 class Main extends Component {
-    state = {
-        tiled: false,
-        RendererSelectedOption: "cpp",
-        ThreeDSelectionOption: "2D",
-        ThreeDSelectionParam: false,
-        ThemeSelectionOption: "Customize",
-        ColorsSelectionOption: "MediumGradient",
-        ColorShiftValue: "0",
-        DarkenTexture: true,
-        SmoothTexture: false,
-        StringLightsTexture: false,
-        FancifulTexture: false,
-        ShadowTexture: false,
-        RoundedEdgesTexture: false,
-        EdgeSelectionOption: "Curved",
-        DepthValue: "30",
-        Morph1Value: "50",
-        Morph2Value: "50",
-        DarkMorphValue: "50",
-        ZAdjustValue: "50",
-        MotionSelectionOption: "Position"
-    }
-
-    inputChangedHandler = (event) => {
-        this.setState({ tiled: !this.state.tiled })
-    }
 
     render() {
 
