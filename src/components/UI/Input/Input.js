f0b116c0d20cd4319e15d63f7689f88b592e0053
Hisni <hisnionline@gmail.com>
2020-03-24 19:21:20 +0530

    Basic Layout Added

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -24,6 +24,7 @@ const input = ( props ) => {
                 className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
+                checked={props.check}
                 onChange={props.changed} />;
             break;
         case ( 'textarea' ):
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+
+import Controls from './Controls/Controls';
 import './Home.css';
 
 class Main extends Component {
 
-    componentDidMount(){
-        // this.props.onTryAutoSignup();
+    state = {
+        tiled: false,
+        RendererSelectedOption: "cpp",
+        ThreeDSelectionOption: "2D",
+        ThreeDSelectionParam: false,
+        ThemeSelectionOption: "Customize",
+        ColorsSelectionOption: "MediumGradient",
+        ColorShiftValue: "0",
+        DarkenTexture: true,
+        SmoothTexture: false,
+        StringLightsTexture: false,
+        FancifulTexture: false,
+        ShadowTexture: false,
+        RoundedEdgesTexture: false,
+        EdgeSelectionOption: "Curved",
+        DepthValue: "30",
+        Morph1Value: "50",
+        Morph2Value: "50",
+        DarkMorphValue: "50",
+        ZAdjustValue: "50",
+        MotionSelectionOption: "Position"
+    }
+
+    inputChangedHandler = (event) => {
+        this.setState({ tiled: !this.state.tiled })
     }
 
     render() {
@@ -12,6 +37,11 @@ class Main extends Component {
         return (
             <div className="App">
                 <h1>Control + Fractal Image</h1>
+                <Controls
+                
+                
+                
+                />
             </div>
         );
     }
